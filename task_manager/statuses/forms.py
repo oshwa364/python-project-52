6859a93195bd4ab98475f0aeb7b6a4b8ed46f89d
Artyom Rozhkov <argue364@gmail.com>
2026-05-29 23:34:16 +0200

    fix: remove not required

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -4,11 +4,6 @@ from .models import Status
 
 
 class StatusForm(forms.ModelForm):
-
-    name = forms.CharField(
-        max_length=200, required=True, label='Имя'
-    )
-
     class Meta:
         model = Status
         fields = ('name',)
