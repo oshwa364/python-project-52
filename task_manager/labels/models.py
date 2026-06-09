2dab13affe6fb439ca315b2abd4947fc90f609ea
Artyom Rozhkov <argue364@gmail.com>
2026-06-09 21:02:30 +0200

    add labels app

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,2 +1,18 @@
+from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        blank=False,
+        verbose_name='Имя'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Label'
+
+    def __str__(self):
+        return self.name
--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,2 +1,50 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-# Create your views here.
+from .models import Label
+from .forms import LabelForm
+
+
+class LabelListView(ListView):
+    model = Label
+    template_name = 'labels/labels.html'
+    context_object_name = 'labels'
+    extra_context = {
+        'title': 'Метки'
+    }
+
+
+class LabelCreateView(CreateView):
+     model = Label
+     form_class = LabelForm
+     template_name = 'form.html'
+     success_url = reverse_lazy('labels_list')
+     success_message = 'Метка успешно создана'
+     extra_context = {
+         'title': 'Создать метку',
+         'button_text': 'Создать',
+     }
+
+
+class LabelUpdateView(UpdateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('labels_list')
+    success_message = 'Метка успешно изменена'
+    extra_context = {
+        'title': 'Редактирование метки',
+        'button_text': 'Изменить',
+    }
+
+
+class LabelDeleteView(DeleteView):
+    model = Label
+    template_name = 'labels/delete.html'
+    success_url = reverse_lazy('labels_list')
+    success_message = 'Метка успешно удалена'
+    extra_context = {
+        'title': 'Удаление метки',
+        'button_text': 'Да, удалить'
+    }
