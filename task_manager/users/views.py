c8d711313f95530842e485d3b3c3f79474f76ef7
Artyom Rozhkov <argue364@gmail.com>
2026-06-11 00:31:51 +0200

    fix troubles

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from task_manager.users.models import User
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
